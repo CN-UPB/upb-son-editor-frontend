50e5b4357cc2750be0b98faecc283aa8aa51e043
Surendra Kulkarni <surendra@mail.uni-paderborn.de>
2016-09-05 15:30:14 +0200

    adding toggle functionality to 'Endpoint' panel

--- js/togglePanel.js
+++ js/togglePanel.js
@@ -6,7 +6,8 @@ $(document).ready(function(){
     */
 
 	$('#toggle-panel-left').click(function(){
-		$('#show-ns').slideUp('slow', function() { 
+		$('#show-ns').slideUp('slow', function() {
+			$('#show-endpoints').slideUp('slow');
 			$('#show-vnfs').slideToggle('slow');
         });
 		$(this).toggleClass("active");
@@ -14,9 +15,16 @@ $(document).ready(function(){
 	
 	$('#toggle-panel-left-ns').click(function(){
         $('#show-vnfs').slideUp('slow', function() {
+			$('#show-endpoints').slideUp('slow');
 			$('#show-ns').slideToggle('slow');
         });
 		$(this).toggleClass("active");
     });
-    
+    $('#toggle-panel-left-endpoints').click(function(){
+		$('#show-vnfs').slideUp('slow', function() {
+			$('#show-ns').slideUp('slow');
+			$('#show-endpoints').slideToggle('slow');
+		});	
+		$(this).toggleClass("active");
+	});
 });
