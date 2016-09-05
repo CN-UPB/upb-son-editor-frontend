ab686f0592a305fe595487c014df1b460210d669
Linghui Luo <linghui@mail.upb.de>
2016-09-05 12:45:01 +0200

    update workspace configuration view

--- js/configuration.js
+++ js/configuration.js
@@ -1,10 +1,52 @@
 var serverURL = "http://131.234.73.193:5000/";
 var queryString = {};
+var workspaceModel;
 
-$(document).ready(function () {
-	queryString = getQueryString();
-	document.getElementById("nav_workspace").text = "Workspace: " + queryString["wsName"];
-});
+var Platform=function()
+{
+	this.platformName=ko.observable("");
+	this.platformURL=ko.observable("");
+	this.init=function(data)
+	{
+		this.platformName(data.platformName);
+		this.platformURL(data.platformURL);
+		return this;
+	}
+}
+
+var Catalogue=function()
+{
+	this.catalogueName=ko.observable("");
+	this.catalogueURL=ko.observable("");
+	this.init=function(data)
+	{
+		this.catalogueName(data.catalogueName);
+		this.catalogueURL(data.catalogueURL);
+		return this;
+	}
+}
+var WorkspaceModel=function()
+{
+	this.name=ko.observable(queryString["wsName"]);
+	this.platforms=ko.observableArray([new Platform()]);
+	this.catalogues=ko.observableArray([new Catalogue()]);
+	this.addPlatform=function()
+	{
+		this.platforms.push(new Platform());
+	};
+	this.deletePlatform=function(platform)
+	{
+		this.platforms.remove(platform);
+	}.bind(this);
+	this.addCatalogue=function()
+	{
+		this.catalogues.push(new Catalogue());
+	};
+	this.deleteCatalogue=function(catalogue)
+	{
+		this.catalogues.remove(catalogue);
+	}.bind(this);
+}
 
 function cancelConfiguration() {
 	window.location.href = history.back();
@@ -12,3 +54,36 @@ function cancelConfiguration() {
 function saveConfiguration() {
 	window.location.href = history.back();
 }
+
+$(document).ready(function () {
+	queryString = getQueryString();
+	document.getElementById("nav_workspace").text = "Workspace: " + queryString["wsName"];
+	workspaceModel=new WorkspaceModel();
+	$.ajax({
+		url : serverURL + "workspaces/" + queryString["wsId"]+"/platforms/",
+		dataType : "json",
+		xhrFields : {
+			withCredentials : true
+		},
+		success : function (data) {
+			workspaceModel.platforms($.map(data, function(item){return new Platform().init(item)}));
+		}
+	});
+	$.ajax({
+		url : serverURL + "workspaces/" + queryString["wsId"]+"/catalogues/",
+		dataType : "json",
+		xhrFields : {
+			withCredentials : true
+		},
+		success : function (data) {
+			workspaceModel.catalogues($.map(catalogues, function(item){return new Catalogue().init(item)}));
+		}
+	});
+	ko.applyBindings(workspaceModel);
+	$("#accordion").accordion({
+		active : false,
+		collapsible : false,
+		heightStyle : "content"
+	});
+});
+
