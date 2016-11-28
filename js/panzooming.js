fd89bf50869d48b2b09c9eddf51feb4b05cc0919
Surendra Kulkarni <surendra@mail.uni-paderborn.de>
2016-11-28 10:46:10 +0100

    modifying panzoom.js and nsEditorStyle.css, adding few changes for zoom and pan.

--- js/panzooming.js
+++ js/panzooming.js
@@ -1,12 +1,20 @@
 	    $(function() {
 
           var $section = $('#editor-parent');
+          //var $in_section = $('#editor');
           var $menu_actions = $('#menu-actions');
           var $panzoom = $section.find('#editor').panzoom({
+            disablePan: true,
             $zoomIn: $menu_actions.find(".zoom-in"),
             $zoomOut: $menu_actions.find(".zoom-out"),
             $reset: $menu_actions.find(".reset")
+            //panOnlyWhenZoomed: true,
+            //minScale: 0.5
+            //contain: true
           });
+          /*var $panzoom_vnf = $in_section.find('#vnf_vnf-sample_0').panzoom({
+            contain: true
+          });*/
 		  $(".editor-area").css("cursor","default");
           $panzoom.on('mousewheel.focal', function( e ) {
             e.preventDefault();
