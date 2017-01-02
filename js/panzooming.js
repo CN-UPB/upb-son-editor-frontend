0325c7d1cc446b216f0893463701c26e9085c7a8
Surendra Kulkarni <surendra@mail.uni-paderborn.de>
2017-01-02 13:36:40 +0100

    adding initial code for zoom

---
+++ js/panzooming.js
@@ -0,0 +1,28 @@
+	    $(function() {
+
+          var $section = $('#editor-parent');
+          //var $in_section = $('#editor');
+          var $menu_actions = $('#menu-actions');
+          var $panzoom = $section.find('#editor').panzoom({
+            //disablePan: true,
+            $zoomIn: $menu_actions.find(".zoom-in"),
+            $zoomOut: $menu_actions.find(".zoom-out"),
+            $reset: $menu_actions.find(".reset")
+            //panOnlyWhenZoomed: true,
+            //minScale: 0.5
+            //contain: true
+          });
+          /*var $panzoom_vnf = $in_section.find('#vnf_vnf-sample_0').panzoom({
+            contain: true
+          });*/
+		  $(".editor-area").css("cursor","default");
+          $panzoom.parent().on('mousewheel.focal', function( e ) {
+            e.preventDefault();
+            var delta = e.delta || e.originalEvent.wheelDelta;
+            var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
+            $panzoom.panzoom('zoom', zoomOut, {
+              animate: false,
+              focal: e
+            });
+          });
+        });
